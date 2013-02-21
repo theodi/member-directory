2cd2080575742c8bbdd2e323200738d5141d8f88
Tom Heath <tom.heath@theodi.org>
2013-02-21 15:59:45 +0000

    added capybara tests for views and started adding appropriate fields

--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -7,23 +7,40 @@ Given /^that I want to sign up$/ do
 end
 
 When /^I visit the signup page$/ do
-  # We don't have a signup page yet!
+  visit('/members/sign_up')
+  page.should have_content 'Sign up'  
 end
 
 When /^I enter my details$/ do
-  @organisation_name     = 'FooBar Inc'
-  @contact_name          = 'Ian McIain' # Required
-  @email                 = 'iain@foobar.com' # Required
-  @phone                 = '0121 123 446'
-  @address_line1         = '123 Fake Street' # Required
-  @address_line2         = 'Fake place'
-  @address_city          = 'Faketown' # Required
-  @address_region        = 'Fakeshire'
-  @address_country       = 'UK' # Required
-  @address_postcode      = 'FAKE 123'
-  @tax_number            = '213244343'
-  @purchase_order_number = 'PO-43243242342'
-  @agreed_to_terms       = true
+  #@organisation_name     = 'FooBar Inc'
+  #@contact_name          = 'Ian McIain' # Required
+  #@email                 = 'iain@foobar.com' # Required
+  #@phone                 = '0121 123 446'
+  #@address_line1         = '123 Fake Street' # Required
+  #@address_line2         = 'Fake place'
+  #@address_city          = 'Faketown' # Required
+  #@address_region        = 'Fakeshire'
+  #@address_country       = 'UK' # Required
+  #@address_postcode      = 'FAKE 123'
+  #@tax_number            = '213244343'
+  #@purchase_order_number = 'PO-43243242342'
+  #@agreed_to_terms       = true
+
+  fill_in('member_contact_name',          :with => 'Ian McIain')
+  fill_in('member_email',                 :with => 'iain@foobar.com')
+  fill_in('member_organisation_name',     :with => 'FooBar Inc')
+  fill_in('member_phone',                 :with => '0121 123 446')
+  fill_in('member_address_line1',         :with => '123 Fake Street')
+  fill_in('member_address_line2',         :with => 'Fake place')
+  fill_in('member_address_city',          :with => 'Faketown')
+  fill_in('member_address_region',        :with => 'Fakeshire')
+  fill_in('member_address_country',       :with => 'UK')
+  fill_in('member_address_postcode',      :with => 'FAKE 123')
+  fill_in('member_tax_number',            :with => '213244343')
+  fill_in('member_purchase_order_number', :with => 'PO-43243242342')
+  #fill_in('member_agreed_to_terms'        = true)
+
+  
 end
 
 When /^I leave (\w*) blank$/ do |field|
