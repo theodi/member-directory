9171f0ef344713190f2c65b44e5169145cd73fbb
James Smith <james@floppy.org.uk>
2013-02-26 16:15:58 +0000

    Don't display error div if there are no errors

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,13 +1,17 @@
 module DeviseHelper
 
   def devise_error_messages!
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    html = <<-HTML
-      <div class="alert alert-error">
-        <ul class='unstyled'>#{messages}</ul>
-      </div>
-    HTML
-    html.html_safe
+    if resource.errors.empty?
+      ''
+    else
+      messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+      html = <<-HTML
+        <div class="alert alert-error">
+          <ul class='unstyled'>#{messages}</ul>
+        </div>
+      HTML
+      html.html_safe
+    end
   end
 
 end
