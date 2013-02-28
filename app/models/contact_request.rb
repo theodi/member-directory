e5b70eb686bc6f0abae2de1139388bb52a695478
James Smith <james@floppy.org.uk>
2013-02-28 16:52:12 +0000

    Add contact form and enqueueing

--- app/models/contact_request.rb
+++ app/models/contact_request.rb
@@ -3,6 +3,8 @@ class ContactRequest
   include ActiveModel::Conversion
   extend ActiveModel::Naming
   
+  attr_accessor :person_name, :person_affiliation, :person_email, :person_telephone, :person_job_title, :product_name, :comment_text
+
   def initialize(attributes = {})
     attributes.each do |name, value|
       send("#{name}=", value)
@@ -11,6 +13,19 @@ class ContactRequest
   
   def persisted?
     false
+  end
+  
+  def save
+    Resque.enqueue(PartnerEnquiryProcessor, {
+        'name'        => person_name,
+        'affiliation' => person_affiliation,
+        'email'       => person_email,
+        'telephone'   => person_telephone,
+        'job_title'   => person_job_title
+      },
+      { 'name' => product_name },
+      { 'text' => comment_text }
+    )
   end
   
 end
