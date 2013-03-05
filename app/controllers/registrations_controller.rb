aaced83831def610bb2f18577cd5e0cecc2138f8
pezholio <pezholio@gmail.com>
2013-03-05 16:46:49 +0000

    Allow member to edit their organization after signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,11 +4,11 @@ class RegistrationsController < Devise::RegistrationsController
   def check_product_name
     redirect_to 'http://www.theodi.org/join-us' unless %w{supporter member}.include?(params[:level].to_s)
   end
-  
+
   protected
 
   def after_sign_up_path_for(resource)
     '/organizations/edit'
   end
-
+  
 end
