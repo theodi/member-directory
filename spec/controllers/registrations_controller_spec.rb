2aff5fe94cd4e90495bac0eee35b4541f0284650
James Smith <james@floppy.org.uk>
2013-03-06 14:10:34 +0000

    Check that level parameter is set before new signups

---
+++ spec/controllers/registrations_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe RegistrationsController do
+
+  before :each do
+    request.env['devise.mapping'] = Devise.mappings[:member]
+  end
+  
+  it 'should show form for supporter level signups' do
+    get :new, :level => 'supporter'
+    response.should be_success
+  end
+
+  it 'should show form for member level signups' do
+    get :new, :level => 'member'
+    response.should be_success
+  end
+
+  it 'should redirect back to join us page for any other level' do
+    get :new, :level => 'spaceman'
+    response.should be_redirect
+    response.should redirect_to('http://www.theodi.org/join-us')
+  end
+
+  it 'should redirect back to join us page for no level' do
+    get :new
+    response.should be_redirect
+    response.should redirect_to('http://www.theodi.org/join-us')
+  end
+
+end
