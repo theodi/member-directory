da39e6c0db13f9f14995f9e927c6c5d493080e7c
pezholio <pezholio@gmail.com>
2013-03-06 08:52:12 +0000

    Removed @organization.save

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -15,7 +15,6 @@ class OrganizationsController < ApplicationController
     if member_signed_in?
       @organization = Organization.find_by_membership_number(current_member.membership_number)    
       if @organization.update_attributes(params[:organization])
-        @organization.save
         redirect_to '/', :notice => 'Your submission has been added. A more elegant message will go here.'
       else
         render action: "edit"
