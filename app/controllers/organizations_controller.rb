33d92e98f0d6a157e866ca1e44fdcf09fa7105a6
pezholio <pezholio@gmail.com>
2013-03-06 15:19:07 +0000

    Character limit wasn't showing after error

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -15,7 +15,8 @@ class OrganizationsController < ApplicationController
   def update
     if member_signed_in?
       @member = current_member
-      @organization = current_member.organization    
+      @organization = current_member.organization
+      @member.product_name == "supporter" ? @limit = 500 : @limit = 1000  
       if @organization.update_attributes(params[:organization])
         redirect_to root_path, :notice => 'Your submission has been added. A more elegant message will go here.'
       else
