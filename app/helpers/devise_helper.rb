cd835f2d367a3cc73a91311f5d368ca1f49fdf82
James Smith <james@floppy.org.uk>
2013-03-06 13:30:33 +0000

    Replace placeholder text with real values

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -4,4 +4,21 @@ module DeviseHelper
     error_messages(resource)
   end
 
+  def product_name
+    params[:level] || @member.product_name
+  end
+
+  def annual_product_price
+    case product_name
+    when 'supporter'
+      "&pound;540".html_safe
+    when 'member'
+      "&pound;4,800".html_safe
+    when 'sponsor'
+      "&pound;25,000".html_safe      
+    when 'partner'
+      "&pound;50,000".html_safe
+    end
+  end
+
 end
