1650a08b739d1a899a5523ab5489b191196ff5b7
pezholio <pezholio@gmail.com>
2013-03-07 16:49:28 +0000

    Add Description and URL to the Database
    As per the discussion in open-orgn-services/issues#166 these details will be stored in the database, rather than pushed to Capsule

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130306114811) do
+ActiveRecord::Schema.define(:version => 20130307162606) do
 
   create_table "members", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
@@ -39,10 +39,12 @@ ActiveRecord::Schema.define(:version => 20130306114811) do
 
   create_table "organizations", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.integer  "member_id"
     t.string   "logo"
+    t.text     "description"
+    t.string   "url"
   end
 
 end
