ee470a977f431ac88aaf804a2055739e7eb7c109
James Smith <james@floppy.org.uk>
2013-03-08 16:53:17 +0000

    protect check_product_name

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :check_product_name, :only => 'new'
 
+  protected
+
   def check_product_name
     redirect_to 'http://www.theodi.org/join-us' unless %w{supporter member}.include?(params[:level].to_s)
   end
-
-  protected
 
   def after_sign_up_path_for(resource)
     '/organizations/edit'
