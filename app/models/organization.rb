8e70304ce670b1add66e19c3faba4aee4d7bd146
James Smith <james@floppy.org.uk>
2013-03-08 14:55:56 +0000

    validate URL

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -9,6 +9,11 @@ class Organization < ActiveRecord::Base
   validates :description, :presence => true, :on => :update
   validates :description, :length => { :maximum  => 500, :too_long => "Your description cannot be longer than %{count} characters"}, :if => :supporter?
   validates :description, :length => { :maximum  => 1000, :too_long => "Your description cannot be longer than %{count} characters"}, :if => :member?
+
+  # We use both a URL-parsing validator, and a simple regexp here
+  # so that we exclude things like http://localhost, which are valid
+  # but undesirable
+  validates :url, :url => {:allow_nil => true}, :format => {:with => /([a-z]+):\/\/([^\.\/]+?)\.([^\.\/]+?)/, :allow_nil => true}
   
   def member?
     self.member.product_name == "member"
