a6071f542b7fba9acd8e116146d84d766c8acdad
James Smith <james@floppy.org.uk>
2013-03-13 14:34:37 +0000

    Update tests for active flags

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -10,11 +10,20 @@ describe MembersController do
   end
 
   describe "GET 'show'" do
-    it "returns http success" do
-      @member = FactoryGirl.create :member
-      get 'show', :id => @member.membership_number
+
+    it "returns http success if member is active" do
+      member = FactoryGirl.create :member, :cached_active => true
+      get 'show', :id => member.membership_number
       response.should be_success
     end
+
+    it "returns 404 if member is not active" do
+      member = FactoryGirl.create :member, :cached_active => false
+      expect {
+        get 'show', :id => member.membership_number
+      }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
   end
 
 end
