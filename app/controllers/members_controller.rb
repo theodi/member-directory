89d255cf8c478b489664a9b5b15d10e70e4b4358
James Smith <james@floppy.org.uk>
2013-03-13 14:28:14 +0000

    Only show active members
    This flag will be set by Capsule sync.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,13 +1,13 @@
 class MembersController < ApplicationController
 
   def index
-    @organizations = Organization.order(:name)
+    @organizations = Organization.includes(:member).where(:'members.cached_active' => true).order(:name)
   end
 
   def show
     # Get member
     @member = Member.where(:membership_number => params[:id]).first
-    raise ActiveRecord::RecordNotFound and return if @member.nil?
+    raise ActiveRecord::RecordNotFound and return if @member.nil? || @member.cached_active != true
     # Get organization
     @organization = @member.organization
     raise ActiveRecord::RecordNotFound and return if @organization.nil?
