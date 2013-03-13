4eae8f3cf40dbad606c41d7bd75122198e1f867d
James Smith <james@floppy.org.uk>
2013-03-13 11:25:35 +0000

    Add member and organization factories

---
+++ spec/factories/members.rb
@@ -0,0 +1,15 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :member do
+    email             Faker::Internet.email
+    product_name      'member'
+    organisation_name Faker::Company.name
+    contact_name      Faker::Name.name
+    street_address    Faker::Address.street_address
+    address_locality  Faker::Address.city
+    address_country   Faker::Address.country
+    postal_code       Faker::Address.postcode
+    password          'passw0rd'
+  end
+end
