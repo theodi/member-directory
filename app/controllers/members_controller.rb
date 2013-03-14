980ddb2d236ef2142e36dfed0da6d2c2249b3adc
James Smith <james@floppy.org.uk>
2013-03-14 17:03:57 +0000

    extract get_member in MembersController

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,19 +1,26 @@
 class MembersController < ApplicationController
   respond_to :html, :json
 
+  before_filter :get_member, :except => [:index]
+
   def index
     @organizations = Organization.includes(:member).where(:'members.cached_active' => true).order(:name)
     respond_with(@organizations)
   end
 
   def show
-    # Get member
-    @member = Member.where(:membership_number => params[:id]).first
-    raise ActiveRecord::RecordNotFound and return if @member.nil? || @member.cached_active != true
     # Get organization
     @organization = @member.organization
-    raise ActiveRecord::RecordNotFound and return if @organization.nil?
+    raise ActiveRecord::RecordNotFound and return if @organization.nil? || @member.cached_active != true
     respond_with(@organization)
+  end
+  
+  private
+  
+  def get_member
+    # Get member
+    @member = Member.where(:membership_number => params[:id]).first
+    raise ActiveRecord::RecordNotFound and return if @member.nil?
   end
   
 end
