1460c915d5ab43988fc6864ff90c0f622f40dc52
James Smith <james@floppy.org.uk>
2013-03-14 14:57:25 +0000

    Integrate contact requests into member creation action

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,11 +2,30 @@ class RegistrationsController < Devise::RegistrationsController
   before_filter :check_product_name, :only => 'new'
 
   def new
-    super
-    if %w{partner sponsor}.include? params[:level].to_s
-      render :action => 'contact_requests/new'
+    if %w{partner sponsor}.include? @product_name
+      @contact_request = ContactRequest.new
+      @product_name == "sponsor" ? @title = "Sponsor us" : @title = "Partner with us"
+      render 'contact_requests/new'
     else
-      render :action => 'devise/registrations/new'      
+      super
+    end
+  end
+
+  def create
+    if params[:contact_request] 
+      @contact_request = ContactRequest.new(params[:contact_request])
+      @product_name = @contact_request.product_name    
+      respond_to do |format|
+        if @contact_request.save
+          format.html { render "contact_requests/create" }
+          format.json { render json: @contact_request, status: :created, location: @contact_request }
+        else
+          format.html { render "contact_requests/new" }
+          format.json { render json: @contact_request.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      super
     end
   end
 
@@ -29,7 +48,8 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def check_product_name
-    redirect_to 'http://www.theodi.org/join-us' unless %w{supporter member partner sponsor}.include?(params[:level].to_s)
+    @product_name = params[:level].to_s
+    redirect_to 'http://www.theodi.org/join-us' unless %w{supporter member partner sponsor}.include?(@product_name)
   end
 
   def after_sign_up_path_for(resource)
