236bccce90e3b809a4031f86db215c1b3a239ced
James Smith <james@floppy.org.uk>
2013-03-14 17:36:10 +0000

    Update login form for new session path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
   
   def after_sign_in_path_for(resource)
-    edit_member_registration_path
+    edit_member_path(resource)    
   end
 
 end
