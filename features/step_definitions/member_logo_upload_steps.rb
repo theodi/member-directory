9cf15c46fb654d72e911279152b150758dd1f1f5
Tom Heath <tom.heath@theodi.org>
2013-03-14 15:54:13 +0000

    more work on the tests

--- features/step_definitions/member_logo_upload_steps.rb
+++ features/step_definitions/member_logo_upload_steps.rb
@@ -8,32 +8,27 @@ Given /^my organisations logo is in the file "(.*?)"$/ do |logo_source_path|
   @logo_source_path = logo_source_path
 end
 
-Given /^my organisations logo is online at the URL "(.*?)"$/ do |logo_source_url|
-  @logo_source_url = logo_source_url
-end
-
 # upload
 When /^I upload my organisations logo from a local file$/ do
+  pending
   # do carrierwave stuff here
-  uploader = ImageObjectUploader.new
-  uploader.store!(@logo_source_path)
-end
-
-When /^I upload my organisations logo from a URL$/ do
-  pending # do carrierwave stuff here
+  #fill_in('member_organization_attributes_description', :with => @logo_source_path)
 end
 
 # storage
 Then /^it should be stored in the container named "(.*?)" with CDN URI "(.*?)"$/ do |container, cdn_uri|
-  @container = container
-  @cdn_uri = cdn_uri
+  pending
+  #@container = container
+  #@cdn_uri = cdn_uri
 end
 
 # destinations
 Then /^the fullsize logo should be available at the URL "(.*?)"$/ do |logo_url_fullsize|
-  @logo_url_fullsize = logo_url_fullsize
+  pending
+  #@logo_url_fullsize = logo_url_fullsize
 end
 
 Then /^the logo thumbnail should be available at the URL "(.*?)"$/ do |logo_url_thumbnail|
-  @logo_url_thumbnail = logo_url_thumbnail
+  pending
+  #@logo_url_thumbnail = logo_url_thumbnail
 end
