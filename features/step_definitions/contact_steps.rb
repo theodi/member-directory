f7f88a671520b6ca47dc62533b4ef3821d54b6ac
James Smith <james@floppy.org.uk>
2013-03-14 15:22:09 +0000

    Update cucumber feature for URL change

--- features/step_definitions/contact_steps.rb
+++ features/step_definitions/contact_steps.rb
@@ -1,5 +1,5 @@
 When /^I visit the contact page$/ do
-  visit("/contact_requests/new?level=#{@product_name}")
+  visit("/members/sign_up?level=#{@product_name}")
   page.should have_content /Partner with us|Sponsor us/
   @field_prefix = 'contact_request'
 end
