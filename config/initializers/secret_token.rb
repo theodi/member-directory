c0eb623202c17ff9a6d00d6e3f95155acf1d4f6d
James Smith <james@floppy.org.uk>
2013-04-19 08:33:21 +0100

    Remove session secret from version control
    Because duh. Allow setting by env var, or generate a random one if missing (only really for development/test)

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MemberDirectory::Application.config.secret_token = '94949ffc3a55ce7afdd582199e38aa1f826d6843003a732b6ee1a37ace5f7207a8638ad39c9e2f84ab386a7357cf2380297a60bf1fe7db93cf1bae033451e011'
+MemberDirectory::Application.config.secret_token = ENV['SESSION_SECRET_DIRECTORY'] || SecureRandom.hex(32)
