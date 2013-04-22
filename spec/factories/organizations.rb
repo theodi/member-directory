28617b8e99f5aec0b6689517d24c3739d0313ab9
James Smith <james@floppy.org.uk>
2013-04-22 10:54:40 +0100

    Allow organizations to have nil names
    Resolves #95

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -5,6 +5,5 @@ FactoryGirl.define do
     name        { Faker::Company.name }
     description { Faker::Company.catch_phrase }
     url         { Faker::Internet.url }
-    member
   end
 end
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,11 +2,30 @@ require 'spec_helper'
 
 describe Organization do
   
+  before :all do
+    @member = FactoryGirl.create :member
+  end
+  
   it "should strip prefix from twitter handles before saving" do
-    org = FactoryGirl.create :organization, :cached_twitter => "@test1"
+    org = FactoryGirl.create :organization, :cached_twitter => "@test1", :member => @member
     org.cached_twitter.should == "test1"
-    org = FactoryGirl.create :organization, :cached_twitter => "test2"
+    org = FactoryGirl.create :organization, :cached_twitter => "test2", :member => @member
     org.cached_twitter.should == "test2"
   end
   
+  it "cannot create organizations with the same name" do
+    name = Faker::Company.name
+    FactoryGirl.create :organization, :name => name, :member => @member
+    expect {
+      FactoryGirl.create :organization, :name => name, :member => @member
+    }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+  
+  it "can create two organizations with no name" do
+    expect {
+      FactoryGirl.create :organization, :name => nil, :member => @member 
+      FactoryGirl.create :organization, :name => nil, :member => @member
+    }.to change{Organization.count}.by(2)
+  end
+
 end
