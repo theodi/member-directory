ff57cb4936150c29e4fe3c3d6df6a5a4b91dd8a6
James Smith <james@floppy.org.uk>
2013-05-07 08:25:38 +0100

    redirect back to referrer after signout
    Can't use session as it's been destroyed, but referrer does the job. Resolves #108

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,14 @@ class ApplicationController < ActionController::Base
     end
   end
   
+  private
+
   def after_sign_in_path_for(resource)
     member_path(resource)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer || root_path
   end
 
 end
