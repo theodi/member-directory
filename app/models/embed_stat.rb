8fd5a6f21a29269e17e95cbf988b3b658d36849b
pezholio <pezholio@gmail.com>
2014-08-27 13:25:24 +0100

    Make sure referrer is a valid URL

--- app/models/embed_stat.rb
+++ app/models/embed_stat.rb
@@ -1,6 +1,7 @@
 class EmbedStat < ActiveRecord::Base
   has_one :member
 
+  validates :referrer, url: true
+  
   attr_accessible :referrer
-
 end
--- spec/models/embed_stat_spec.rb
+++ spec/models/embed_stat_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe EmbedStat do
 
+  it "returns an error if referrer is an invalid URL" do
+    e = EmbedStat.create(referrer: "this is not a URL")
+
+    e.valid?.should be_false
+    e.errors.first.should == [:referrer, "is not a valid URL"]
+  end
+
 end
