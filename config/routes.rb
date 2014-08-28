4fe1f95880a04171279e11e1ac09223f397ab81b
pezholio <pezholio@gmail.com>
2014-08-28 11:21:31 +0100

    Add embed_stats controller method

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ MemberDirectory::Application.routes.draw do
   root :to => redirect("/members")
 
   get '/logos/:level/:size/:colour.svg', defaults: {format: :svg}, to: 'application#logo'
+  get 'embed_stats.csv', to: 'embed_stats#index'
 
   match '/401', :to => 'errors#unauthorized'
   match '/404', :to => 'errors#not_found'
