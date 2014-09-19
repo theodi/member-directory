0e97db33dfe0cae202955adc5a57bd1f73ef456d
James Smith <james@floppy.org.uk>
2014-09-19 15:24:34 +0100

    Test password reset and fix bug

---
+++ features/step_definitions/password_reset_steps.rb
@@ -0,0 +1,30 @@
+When(/^I request a password reset$/) do
+  visit '/'
+  click_link 'Sign in'
+  click_link 'Forgot your password?'
+  fill_in 'member_email', with: @membership.email
+  click_button 'Send me reset password instructions'
+end
+
+Then(/^I should receive a password reset email$/) do
+  steps %Q(
+    Then "#{@membership.email}" should receive an email with subject "Password reset instructions"
+    When I open the email with subject "Password reset instructions"
+    Then I should see "Someone has requested a link to change your password." in the email body
+    And I should see the email delivered from "members@theodi.org"
+  )
+end
+
+Then(/^I should be able to change my password$/) do
+  steps %Q(
+    When I click the first link in the email
+    Then I should see "Set your password"
+  )
+  @new_password = 'nyarlathotep'
+  fill_in 'member_password', with: @new_password
+  fill_in 'member_password_confirmation', with: @new_password
+  click_button 'Change my password'
+  steps %Q(
+    Then I should see "Your password was changed successfully. You are now signed in."
+  )
+end
