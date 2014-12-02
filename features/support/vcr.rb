ce6e46f96debe58a44b8be9f1ff86e9b4f393fe2
pezholio <pezholio@gmail.com>
2014-12-02 16:07:53 +0000

    Add comprehensive ENV filtering

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -3,11 +3,7 @@ require 'webmock/cucumber'
 
 VCR.configure do |c|
   # Automatically filter all secure details that are stored in the environment
-  ignore_env = %w(
-    SHLVL RUNLEVEL GUARD_NOTIFY DRB COLUMNS USER LOGNAME
-    LINES ITERM_PROFILE TODO AUTOFEATURE RAILS_ENV
-  )
-  (ENV.keys - ignore_env).select { |x| x =~ /\A[A-Z_]*\Z/ }.each do |key|
+  (ENV.keys - $ignore_env).select { |x| x =~ /\A[A-Z_]*\Z/ }.each do |key|
     c.filter_sensitive_data("<#{key}>") { ENV[key] }
   end
   c.default_cassette_options = { record: :once }
