022eabfa74e53ba6996e395f063f708b380372bb
Adam Sven Johnson <adam@pkqk.net>
2014-12-17 15:47:02 +0000

    Include as attachments the T&Cs and data protection policy
    not worked out how to test this yet as it’s digging into rails/devise in a dirty way :/
    
    This is where I found out how to render templates to strings :(
    https://web.archive.org/web/20130528180257/http://geek.swombat.com/rails-rendering-templates-outside-of-a-contro

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -10,4 +10,19 @@ class DeviseMailer < Devise::Mailer
     headers
   end
 
+  def confirmation_instructions(record, opts)
+    if record.individual?
+      document_renderer = DocumentRenderer.new
+      attachments['terms-and-conditions.html'] = {
+        mime_type: 'text/html',
+        content: document_renderer.terms_and_conditions(record)
+      }
+      attachments['data-protection-policy.html'] = {
+        mime_type: 'text/html',
+        content: document_renderer.data_protection_policy(record)
+      }
+    end
+    super
+  end
+
 end
