bed26510185a7c3a96a5c622ade8f2c254483108
pezholio <pezholio@gmail.com>
2014-12-18 14:43:30 +0000

    Revert "Allow rackspace connections further up the chain"
    This reverts commit ccf5271bf223b6c6ab97b13b6fedb24295fddc23.

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -1,8 +1,6 @@
 require 'vcr'
 require 'webmock/cucumber'
 
-WebMock.disable_net_connect!(:allow => 'lon.auth.api.rackspacecloud.com')
-
 VCR.configure do |c|
   # Automatically filter all secure details that are stored in the environment
   ignore_env = %w(
@@ -16,6 +14,7 @@ VCR.configure do |c|
   c.cassette_library_dir = 'fixtures/vcr_cassettes'
   c.hook_into :webmock
   c.ignore_localhost = true
+  c.ignore_hosts 'lon.auth.api.rackspacecloud.com'
   c.ignore_request do |request|
     URI(request.uri).host =~ %r{.*\.clouddrive\.com.*}
   end
