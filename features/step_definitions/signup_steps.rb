d60487b2d3ad370eeaf0a36428ce9d5572f16656
Adam Sven Johnson <adam@pkqk.net>
2014-12-18 00:00:38 +0000

    Remove full stop as sentence is now in a list

--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -183,7 +183,7 @@ Then /^a welcome email should be sent to me$/ do
     When they open the email
     And they should see the email delivered from "members@theodi.org"
     And they should see "Welcome to the ODI network!" in the email subject
-    And they should see "Your membership number is <strong>#{@membership_number}</strong>." in the email body
+    And they should see "Your membership number is <strong>#{@membership_number}</strong>" in the email body
     And they should see "Georgia, Carl, Andrea, Clara and Patrik" in the email body
     And they should see "mailto:members@theodi.org" in the email body
   }
