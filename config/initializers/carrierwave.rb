0c0f922a44b8e2ee97504c49ed87e30946ffab3b
Adam Sven Johnson <adam@pkqk.net>
2015-02-20 17:33:19 +0000

    Only set up Fog if rackspace settings are present
    so that tests can run without trying to connect

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,12 @@
-
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider           => 'Rackspace',
-    :rackspace_username => ENV['RACKSPACE_USERNAME'],
-    :rackspace_api_key  => ENV['RACKSPACE_API_KEY'],
-    :rackspace_auth_url => ENV['RACKSPACE_API_ENDPOINT']
-  }
-  config.fog_directory = ENV['RACKSPACE_DIRECTORY_CONTAINER']
-  config.asset_host = ENV['RACKSPACE_DIRECTORY_ASSET_HOST']
+if ENV['RACKSPACE_USERNAME'].present?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :provider           => 'Rackspace',
+      :rackspace_username => ENV['RACKSPACE_USERNAME'],
+      :rackspace_api_key  => ENV['RACKSPACE_API_KEY'],
+      :rackspace_auth_url => ENV['RACKSPACE_API_ENDPOINT']
+    }
+    config.fog_directory = ENV['RACKSPACE_DIRECTORY_CONTAINER']
+    config.asset_host = ENV['RACKSPACE_DIRECTORY_ASSET_HOST']
+  end
 end
