204f296670d04f7fb691c07dc76b824f3195ba36
Jordan Elver <jordan.elver@gmail.com>
2015-08-28 14:21:49 +0100

    Add spec for UpdateDirectoryEntry
    Decided to consolidate the DirectoryEntryUpdate object back into this
    one. Seems to make sense in the end and made the UpdateDirectoryEntry
    object less complicated. The tests spoke and I listened :)

--- app/models/update_directory_entry.rb
+++ app/models/update_directory_entry.rb
@@ -1,24 +1,35 @@
 class UpdateDirectoryEntry
   extend Forwardable
 
-  def_delegators :directory_entry_update,
-    :membership_number,
-    :organization_name,
-    :directory_entry,
-    :update_date
+  def_delegators :organization,
+    :name,
+    :description,
+    :url,
+    :logo,
+    :cached_contact_name,
+    :cached_contact_phone,
+    :cached_contact_email,
+    :cached_twitter,
+    :cached_linkedin,
+    :cached_facebook,
+    :cached_tagline,
+    :updated_at,
+    :member
 
   attr_reader :organization
+  attr_reader :queue
 
   def self.update!(organization)
     new(organization).update!
   end
 
-  def initialize(organization)
+  def initialize(organization, queue = Resque)
     @organization = organization
+    @queue        = queue
   end
 
   def update!
-    Resque.enqueue(SendDirectoryEntryToCapsule,
+    queue.enqueue(SendDirectoryEntryToCapsule,
       membership_number,
       organization_name,
       directory_entry,
@@ -26,8 +37,42 @@ class UpdateDirectoryEntry
     )
   end
 
-  def directory_entry_update
-    @directory_entry_update ||= DirectoryEntryUpdate.new(organization)
+  def update_date
+    updated_at.to_s
+  end
+
+  def membership_number
+    member.membership_number
+  end
+
+  def organization_name
+    {
+      :name => name
+    }
+  end
+
+  def directory_entry
+    {
+      :description => description,
+      :homepage    => url,
+      :logo        => logo_url,
+      :thumbnail   => square_url,
+      :contact     => cached_contact_name,
+      :phone       => cached_contact_phone,
+      :email       => cached_contact_email,
+      :twitter     => cached_twitter,
+      :linkedin    => cached_linkedin,
+      :facebook    => cached_facebook,
+      :tagline     => cached_tagline
+    }
+  end
+
+  def logo_url
+    logo.url
+  end
+
+  def square_url
+    logo.square.url
   end
 end
 
