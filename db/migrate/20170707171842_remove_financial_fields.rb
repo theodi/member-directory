ce790e02a7f876da5213339affdaaa455b45ab39
James Smith <james@floppy.org.uk>
2017-07-11 13:57:55 +0100

    fix migrations

--- db/migrate/20170707171842_remove_financial_fields.rb
+++ db/migrate/20170707171842_remove_financial_fields.rb
@@ -1,12 +1,12 @@
 class RemoveFinancialFields < ActiveRecord::Migration
   def change
-    remove_column :members, :chargify_customer_id, :string
-    remove_column :members, :chargify_subscription_id, :string
-    remove_column :members, :chargify_payment_id, :string
-    remove_column :members, :chargify_data_verified, :boolean, default: false, null: true
-    remove_column :members, :coupon, :string
-    remove_column :members, :payment_frequency, :string, default: 'annual', null: false
-    remove_column :members, :invoice, :boolean, default: false
-    remove_column :members, :organization_vat_id, :string
+    remove_column :members, :chargify_customer_id
+    remove_column :members, :chargify_subscription_id
+    remove_column :members, :chargify_payment_id
+    remove_column :members, :chargify_data_verified
+    remove_column :members, :coupon
+    remove_column :members, :payment_frequency
+    remove_column :members, :invoice
+    remove_column :members, :organization_vat_id
   end
 end
