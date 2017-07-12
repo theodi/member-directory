1d832caaef36e21c0f5094a979f11e275703d8bd
James Smith <james@floppy.org.uk>
2017-07-13 00:06:01 +0100

    remove old options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,10 +60,6 @@ MemberDirectory::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
   config.action_mailer.smtp_settings = {
     :user_name => ENV["MANDRILL_USERNAME"],
     :password => ENV["MANDRILL_PASSWORD"],
