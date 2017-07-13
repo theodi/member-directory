15b0151f27f5e2bbff559b900d989cf528124c6c
James Smith <james@floppy.org.uk>
2017-07-13 01:21:48 +0100

    update schema syntax

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -9,116 +8,111 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20170711215520) do
+ActiveRecord::Schema.define(version: 20170711215520) do
 
-  create_table "admins", :force => true do |t|
-    t.string   "email",              :default => "", :null => false
-    t.integer  "sign_in_count",      :default => 0
+  create_table "admins", force: :cascade do |t|
+    t.string "email", limit: 255, default: "", null: false
+    t.integer "sign_in_count", default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
+    t.string "current_sign_in_ip", limit: 255
+    t.string "last_sign_in_ip", limit: 255
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
   end
 
-  add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
-
-  create_table "data_migrations", :id => false, :force => true do |t|
-    t.string "version", :null => false
+  create_table "data_migrations", id: false, force: :cascade do |t|
+    t.string "version", limit: 255, null: false
+    t.index ["version"], name: "data_migrations_unique_data_migrations"
   end
 
-  add_index "data_migrations", ["version"], :name => "data_migrations_unique_data_migrations"
-
-  create_table "embed_stats", :force => true do |t|
-    t.string   "referrer"
-    t.integer  "member_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "embed_stats", force: :cascade do |t|
+    t.string "referrer", limit: 255
+    t.integer "member_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["referrer"], name: "index_embed_stats_on_referrer", unique: true
   end
 
-  add_index "embed_stats", ["referrer"], :name => "index_embed_stats_on_referrer", :unique => true
-
-  create_table "listings", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-    t.integer  "member_id"
-    t.string   "logo"
-    t.text     "description"
-    t.string   "url"
-    t.string   "contact_name"
-    t.string   "contact_phone"
-    t.string   "contact_email"
-    t.string   "twitter"
-    t.string   "facebook"
-    t.string   "linkedin"
-    t.string   "tagline"
+  create_table "listings", force: :cascade do |t|
+    t.string "name", limit: 255
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "member_id"
+    t.string "logo", limit: 255
+    t.text "description"
+    t.string "url", limit: 255
+    t.string "contact_name", limit: 255
+    t.string "contact_phone", limit: 255
+    t.string "contact_email", limit: 255
+    t.string "twitter", limit: 255
+    t.string "facebook", limit: 255
+    t.string "linkedin", limit: 255
+    t.string "tagline", limit: 255
   end
 
-  create_table "members", :force => true do |t|
-    t.string   "email",                                    :default => "",      :null => false
-    t.string   "encrypted_password",                       :default => "",      :null => false
-    t.string   "reset_password_token"
+  create_table "members", force: :cascade do |t|
+    t.string "email", limit: 255, default: "", null: false
+    t.string "encrypted_password", limit: 255, default: "", null: false
+    t.string "reset_password_token", limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                            :default => 0
+    t.integer "sign_in_count", default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                                    :null => false
-    t.datetime "updated_at",                                                    :null => false
-    t.string   "membership_number",           :limit => 8
-    t.string   "product_name"
-    t.boolean  "active",                                   :default => false
-    t.boolean  "newsletter",                               :default => false
-    t.integer  "embed_stat_id"
-    t.string   "organization_sector"
-    t.string   "organization_size"
-    t.string   "name"
-    t.string   "phone"
-    t.text     "address"
-    t.string   "street_address"
-    t.string   "address_locality"
-    t.string   "address_region"
-    t.string   "address_country"
-    t.string   "postal_code"
-    t.string   "organization_type"
-    t.string   "organization_company_number"
-    t.boolean  "current",                                  :default => false,   :null => false
-    t.string   "origin",                                   :default => "odihq", :null => false
-    t.boolean  "share_with_third_parties",                 :default => false
-    t.string   "twitter"
-    t.string   "organization_name"
-    t.string   "organization_logo"
-    t.text     "organization_description"
-    t.string   "organization_url"
-    t.string   "organization_contact_name"
-    t.string   "organization_contact_phone"
-    t.string   "organization_contact_email"
-    t.string   "organization_twitter"
-    t.string   "organization_facebook"
-    t.string   "organization_linkedin"
-    t.string   "organization_tagline"
+    t.string "current_sign_in_ip", limit: 255
+    t.string "last_sign_in_ip", limit: 255
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "membership_number", limit: 8
+    t.string "product_name", limit: 255
+    t.boolean "active", default: false
+    t.boolean "newsletter", default: false
+    t.integer "embed_stat_id"
+    t.string "organization_sector", limit: 255
+    t.string "organization_size", limit: 255
+    t.string "name", limit: 255
+    t.string "phone", limit: 255
+    t.text "address"
+    t.string "street_address", limit: 255
+    t.string "address_locality", limit: 255
+    t.string "address_region", limit: 255
+    t.string "address_country", limit: 255
+    t.string "postal_code", limit: 255
+    t.string "organization_type", limit: 255
+    t.string "organization_company_number", limit: 255
+    t.boolean "current", default: false, null: false
+    t.string "origin", limit: 255, default: "odihq", null: false
+    t.boolean "share_with_third_parties", default: false
+    t.string "twitter", limit: 255
+    t.string "organization_name", limit: 255
+    t.string "organization_logo", limit: 255
+    t.text "organization_description"
+    t.string "organization_url", limit: 255
+    t.string "organization_contact_name", limit: 255
+    t.string "organization_contact_phone", limit: 255
+    t.string "organization_contact_email", limit: 255
+    t.string "organization_twitter", limit: 255
+    t.string "organization_facebook", limit: 255
+    t.string "organization_linkedin", limit: 255
+    t.string "organization_tagline", limit: 255
+    t.index ["email"], name: "index_members_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
 
-  add_index "members", ["email"], :name => "index_members_on_email", :unique => true
-  add_index "members", ["reset_password_token"], :name => "index_members_on_reset_password_token", :unique => true
-
-  create_table "rails_admin_histories", :force => true do |t|
-    t.text     "message"
-    t.string   "username"
-    t.integer  "item"
-    t.string   "table"
-    t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 8
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
+  create_table "rails_admin_histories", force: :cascade do |t|
+    t.text "message"
+    t.string "username", limit: 255
+    t.integer "item"
+    t.string "table", limit: 255
+    t.integer "month", limit: 2
+    t.integer "year", limit: 8
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item", "table", "month", "year"], name: "index_rails_admin_histories"
   end
-
-  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
 end
